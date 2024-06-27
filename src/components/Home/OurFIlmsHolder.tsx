dfa5df1450ca692de274cb020a03c1d68decc76d
Sumrit Grover <sumritgrover1@gmail.com>
2024-06-27 09:28:49 +0530

    testing

--- src/components/Home/OurFIlmsHolder.tsx
+++ src/components/Home/OurFIlmsHolder.tsx
@@ -1,6 +1,8 @@
 import OurFilms from '@/src/components/Home/Films';
 import useFilms from '@/src/hooks/useFilms';
 
+export const revalidate = 5;
+
 const OurFilmsHolder = async () => {
   const { fetchFilms } = useFilms();
   const filmsData = await fetchFilms();
