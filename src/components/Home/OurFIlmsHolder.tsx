6ab9b428f01b5655ba476ec8ae1d2aac06505727
Sumrit Grover <sumritgrover1@gmail.com>
2024-06-27 11:19:52 +0530

    removed revalidate

--- src/components/Home/OurFIlmsHolder.tsx
+++ src/components/Home/OurFIlmsHolder.tsx
@@ -1,8 +1,6 @@
 import OurFilms from '@/src/components/Home/Films';
 import useFilms from '@/src/hooks/useFilms';
 
-export const revalidate = 5;
-
 const OurFilmsHolder = async () => {
   const { fetchFilms } = useFilms();
   const filmsData = await fetchFilms();
